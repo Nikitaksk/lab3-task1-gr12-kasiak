f9c72bce0ef3d98ff8ae4ba5bb889123a774c883
Nikitaksk <48561848+Nikitaksk@users.noreply.github.com>
2023-03-14 12:38:55 +0000

    formatted & solved

--- src/main.c
+++ src/main.c
@@ -10,9 +10,10 @@ int sum_of_digits(int num) {
     return sum;
 }
 
-int compare_integers(const void* a, const void* b) {
-    int sum_a = sum_of_digits(*(int*)a);
-    int sum_b = sum_of_digits(*(int*)b);
+int compare_integers(const void * a,
+    const void * b) {
+    int sum_a = sum_of_digits( * (int * ) a);
+    int sum_b = sum_of_digits( * (int * ) b);
     if (sum_a < sum_b) {
         return -1;
     } else if (sum_a > sum_b) {
@@ -25,26 +26,26 @@ int compare_integers(const void* a, const void* b) {
 int main() {
     int n;
     printf("Enter the size of the array: ");
-    scanf("%d", &n);
-    
+    scanf("%d", & n);
+
     int arr[n];
 
     for (int i = 0; i < n; i++) {
         arr[i] = rand() % 100;
     }
-  printf("Generated array is:\n");
+    printf("Generated array is:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-    
+
     qsort(arr, n, sizeof(int), compare_integers);
-    
+
     printf("The sorted array is:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
