56bf352af9e0488ca1562e1e90358f3a50399a30
Nikitaksk <48561848+Nikitaksk@users.noreply.github.com>
2023-03-14 12:37:28 +0000

    task solved

--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-#include <time.h>
 
 int sum_of_digits(int num) {
     int sum = 0;
@@ -26,33 +23,28 @@ int compare_integers(const void* a, const void* b) {
 }
 
 int main() {
-    srand(time(NULL));
-
     int n;
     printf("Enter the size of the array: ");
-    if (scanf("%d", &n) != 1) {
-    printf("Error reading input.\n");
-    return 1;
-    }
-
+    scanf("%d", &n);
+    
     int arr[n];
-    printf("Enter %d numbers:\n", n);
+
     for (int i = 0; i < n; i++) {
         arr[i] = rand() % 100;
     }
-
-    printf("Array is:\n");
+  printf("Generated array is:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-  qsort(arr, n, sizeof(int), compare_integers);
+    
+    qsort(arr, n, sizeof(int), compare_integers);
     
     printf("The sorted array is:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-
-    free(arr);
+    printf("\n");
+    
     return 0;
 }
