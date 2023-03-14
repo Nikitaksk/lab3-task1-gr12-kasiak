b0f663c2a8a2a9812fb04780ef806f3ad9a0e785
Nikitaksk <48561848+Nikitaksk@users.noreply.github.com>
2023-03-14 12:15:02 +0000

    makefile added

--- .ccls-cache/@home@runner@lab3-task1-gr12-kasiak/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr12-kasiak/src@main.c
@@ -1,8 +1,30 @@
-/* hello.c */
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+#include <stdlib.h>
+
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    int n;
+    printf("Enter the size of the array: ");
+    if (scanf("%d", &n) != 1) {
+    printf("Error reading input.\n");
+    return 1;
+    }
+
+    int arr[n];
+    printf("Enter %d numbers:\n", n);
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+
+    printf("Array is:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
