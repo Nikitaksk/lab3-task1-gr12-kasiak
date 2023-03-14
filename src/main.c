fc587c3c99e294264f710e95b2237e5e731f66c7
Nikitaksk <48561848+Nikitaksk@users.noreply.github.com>
2023-03-14 12:28:19 +0000

    sorting added

--- src/main.c
+++ src/main.c
@@ -4,6 +4,27 @@
 
 #include <time.h>
 
+int sum_of_digits(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int compare_integers(const void* a, const void* b) {
+    int sum_a = sum_of_digits(*(int*)a);
+    int sum_b = sum_of_digits(*(int*)b);
+    if (sum_a < sum_b) {
+        return -1;
+    } else if (sum_a > sum_b) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main() {
     srand(time(NULL));
 
@@ -25,6 +46,13 @@ int main() {
         printf("%d ", arr[i]);
     }
     printf("\n");
+  qsort(arr, n, sizeof(int), compare_integers);
+    
+    printf("The sorted array is:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
+    free(arr);
     return 0;
 }
